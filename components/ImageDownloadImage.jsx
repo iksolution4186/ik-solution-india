e80f0cd82db9f8859cd1bfe4f8bb5c9c1af320d9
kuldeep badhan <kuldeepbadhan781@gmail.com>
2023-04-24 12:57:34 +0530

    add image download fn by opening it in new tab

--- components/ImageDownloadImage.jsx
+++ components/ImageDownloadImage.jsx
@@ -3,13 +3,20 @@ import React from "react";
 function ImageDownloadButton({ imageUrl, fileName }) {
   const handleDownload = async () => {
     try {
-      const xhr = new XMLHttpRequest();
-      xhr.responseType = "blob";
-      xhr.onload = (event) => {
-        const blob = xhr.response;
-      };
-      xhr.open("GET", imageUrl);
-      xhr.send();
+      // const xhr = new XMLHttpRequest();
+      // xhr.responseType = "blob";
+      // xhr.onload = (event) => {
+      //   const blob = xhr.response;
+      // };
+      // xhr.open("GET", imageUrl);
+      // xhr.send();
+      const link = document.createElement("a");
+      link.href = imageUrl;
+      link.setAttribute("download", "");
+      link.setAttribute("target", "_blank");
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
     } catch (error) {
       console.error(error);
     }
